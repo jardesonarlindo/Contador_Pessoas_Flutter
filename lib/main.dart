9d53cc46f0d174c82ab0669a72d84ff60b93fd6a
Jardeson Arlindo <jardesonaraujoarlindo@gmail.com>
2021-01-19 16:48:12 -0300

    Aula 02 Padding

--- lib/main.dart
+++ lib/main.dart
@@ -9,23 +9,24 @@ void main() {
           Text("Pessoas: 0",
               style:
                   TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              FlatButton(
-                  onPressed: () {},
-                  child: Text(
-                    "+1",
-                    style: TextStyle(fontSize: 40.0, color: Colors.white),
-                  )),
-              FlatButton(
-                  onPressed: () {},
-                  child: Text(
-                    "-1",
-                    style: TextStyle(fontSize: 40.0, color: Colors.white),
-                  )),
-            ],
-          ),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            Padding(
+                padding: EdgeInsets.all(10.0),
+                child: FlatButton(
+                    onPressed: () {},
+                    child: Text(
+                      "+1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
+                    ))),
+            Padding(
+                padding: EdgeInsets.all(10.0),
+                child: FlatButton(
+                    onPressed: () {},
+                    child: Text(
+                      "-1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
+                    ))),
+          ]),
           Text("Pode Entrar!",
               style:
                   TextStyle(color: Colors.white, fontWeight: FontWeight.bold))
